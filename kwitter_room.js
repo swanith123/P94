da1622d4c963f3279652fd71c4535e35a8077fe8
swanith123 <73668844+swanith123@users.noreply.github.com>
2021-09-28 18:32:27 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCsgh_1wbAWOr3maJM3yyMlyP7OIz7piOE",
+    authDomain: "let-chat-web-app-66cf8.firebaseapp.com",
+    projectId: "let-chat-web-app-66cf8",
+    storageBucket: "let-chat-web-app-66cf8.appspot.com",
+    messagingSenderId: "270549214391",
+    appId: "1:270549214391:web:65930265c05dfedb545932",
+    measurementId: "G-6B52B2SNLQ"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
